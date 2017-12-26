b865a584e7f7c36830e00b1b3f0cb85758331831
Novy River <nbhs.kw@gmail.com>
2017-12-26 17:07:07 +0000

    Complete chefchat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require_tree .
 
 function scrollToBottom(){
-  if($('#messages').lencgth>0){
+  if($('#messages').length>0){
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
   }
 }
